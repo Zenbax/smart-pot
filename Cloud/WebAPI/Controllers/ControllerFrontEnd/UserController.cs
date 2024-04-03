a948f1c70bcdccea8d5745d824ee91a3a1127bd5
EQKim <114055097+EQKim@users.noreply.github.com>
2024-04-03 13:38:36 +0200

    Testing controller

--- Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
+++ Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
@@ -6,6 +6,13 @@ namespace Cloud.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
- 
-    
+    [HttpPost]
+    public IActionResult PostUser([FromBody] User user)
+    {
+        if (user == null)
+        {
+            return BadRequest("User is null.");
+        }
+        return CreatedAtAction(nameof(PostUser), new { id = user.Id }, user);
+    }
 }
