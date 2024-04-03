6f056a282cbd9a5139f798b6cf136931b5465baa
EQKim <114055097+EQKim@users.noreply.github.com>
2024-04-03 14:24:10 +0200

    New Stuff

--- Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
+++ Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Threading.Tasks;
 
 namespace Cloud.Controllers;
 
@@ -6,14 +8,32 @@ namespace Cloud.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
+    private readonly IMongoDatabase _database;
+
+    public UserController(IMongoDatabase database)
+    {
+        _database = database;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var usersCollection = _database.GetCollection<User>("Users");
+        var users = await usersCollection.Find(_ => true).ToListAsync();
+        return Ok(users);
+    }
+
+
     [HttpPost]
-    public IActionResult PostUser([FromBody] String text)
+    public IActionResult PostUser([FromBody] string text)
     {
         if (text == null)
         {
             return BadRequest("User is null.");
         }
 
+        // Here you would typically parse the text to a User object and insert it into the database
         return Ok();
     }
 }
