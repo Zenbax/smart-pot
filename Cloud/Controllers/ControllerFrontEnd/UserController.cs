3531f21e980ab36efda8274a198f4799a164a6eb
Zenbax <sebson@outlook.dk>
2024-04-03 14:47:18 +0200

    Now works with docker container

--- Cloud/Controllers/ControllerFrontEnd/UserController.cs
+++ Cloud/Controllers/ControllerFrontEnd/UserController.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace Cloud.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("users")]
 public class UserController : ControllerBase
 {
- 
-    
+    // create the router for the get method, make the route "get-all"
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = "{ \"users\": [ { \"name\": \"John\" }, { \"name\": \"Doe\" } ] };";
+        return Ok(users);
+    }
 }
