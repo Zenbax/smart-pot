18acdccc1da49e4aab8c75226666b0a22292de24
EQKim <114055097+EQKim@users.noreply.github.com>
2024-04-03 14:45:24 +0200

    RebaseFrom Cloud

--- Cloud/ClassLibrary/User.cs
+++
@@ -1,8 +0,0 @@
-﻿public class User
-{
-    // The ObjectId is represented as a string here for simplicity, but you can also use MongoDB.Bson.ObjectId
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
--- Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
+++ Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Driver;
-using System.Threading.Tasks;
 
 namespace Cloud.Controllers;
 
@@ -8,57 +6,14 @@ namespace Cloud.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    private readonly IMongoDatabase _database;
-
-    public UserController(IMongoDatabase database)
-    {
-        _database = database;
-    }
-
-
-    
-    
-    
-
-
-    /*Testing
-    [HttpGet]
-    public async Task<IActionResult> GetAllUsers()
-    {
-        var usersCollection = _database.GetCollection<User>("Users");
-        var users = await usersCollection.Find(_ => true).ToListAsync();
-        return Ok(users);
-    }
-
-    
-    [HttpGet("{name}")]
-    public async Task<IActionResult> GetUserByName(string name)
-    {
-        var usersCollection = _database.GetCollection<User>("users"); 
-        var filter = Builders<User>.Filter.Eq("name", name);
-        var user = await usersCollection.Find(filter).FirstOrDefaultAsync();
-
-        if (user == null)
-        {
-            return NotFound($"User with name: {name} not found.");
-        }
-
-        return Ok(user);
-    }
-
-
-
     [HttpPost]
-    public async Task<IActionResult> PostUser([FromBody] User newUser)
+    public IActionResult PostUser([FromBody] String text)
     {
-        if (newUser == null)
+        if (text == null)
         {
             return BadRequest("User is null.");
         }
-        
-        var usersCollection = _database.GetCollection<User>("users"); // Make sure to use the correct collection name
-        await usersCollection.InsertOneAsync(newUser);
-        return CreatedAtAction(nameof(GetUserByName), new { name = newUser.Name }, new { newUser.Id, newUser.Name, newUser.Email });
+
+        return Ok();
     }
-	*/
 }
