a16ffe0c9b5340faa6ec822909acfc8b40e34125
EQKim <114055097+EQKim@users.noreply.github.com>
2024-04-10 14:06:36 +0200

    Testing MongoDB

--- Cloud/Domain/User.cs
+++ Cloud/Domain/User.cs
@@ -1,7 +1,13 @@
 ﻿namespace ClassLibrary;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 
 public class User
 {
+    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public string Name { get; set; }
     public string LastName { get; set; }
