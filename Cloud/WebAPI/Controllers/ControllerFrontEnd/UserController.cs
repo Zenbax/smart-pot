613ede31752e004b8d69dfa18839095beebee30d
Zenbax <sebson@outlook.dk>
2024-04-12 22:26:09 +0200

    [Fix] GC 64

--- Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
+++ Cloud/WebAPI/Controllers/ControllerFrontEnd/UserController.cs
@@ -9,16 +9,20 @@ namespace YourApiNamespace.Controllers
     public class UserController : ControllerBase
     {
         private readonly IMongoCollection<User> _usersCollection;
+        private readonly ILogger _logger;
 
-        public UserController(IMongoCollection<User> usersCollection)
+        public UserController(IMongoCollection<User> usersCollection, ILogger<UserController> logger)
         {
             _usersCollection = usersCollection;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
-            console.log("This is a TEST!! GET USERS!");
+            _logger.LogInformation("1 This is a TEST!! GET USERS!");
+            _logger.LogWarning("2 This is a TEST!! GET USERS! warn!");
+            _logger.LogError("3 This is a TEST!! GET USERS! err!");
             var users = await _usersCollection.Find(_ => true).ToListAsync();
             return Ok(users);
         }
