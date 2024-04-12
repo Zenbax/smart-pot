a3144961d2f61632cb080ede1892391460cc1e36
Zenbax <sebson@outlook.dk>
2024-04-12 12:36:54 +0200

    [Fix] GC 48

--- Cloud/WebAPI/Program.cs
+++ Cloud/WebAPI/Program.cs
@@ -4,7 +4,6 @@ using MongoDB.Driver;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
 // MongoDB configuration
 var mongoDbSettings = builder.Configuration.GetSection("MongoDB");
 builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
