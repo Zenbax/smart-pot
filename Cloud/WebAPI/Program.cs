c14fa07b9bc653c6ec399924b259a796421848b5
Zenbax <sebson@outlook.dk>
2024-04-12 13:08:39 +0200

    [Fix] GC 51

--- Cloud/WebAPI/Program.cs
+++ Cloud/WebAPI/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddSingleton(userCollection);
 // Add services to the container.
 
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
