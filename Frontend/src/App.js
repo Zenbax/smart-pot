bf6c12d940713ceec1049a0d27fd3ab061313784
Maja Brixen <331282@via.dk>
2024-04-17 11:00:54 +0200

    Node Fix

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,8 +9,8 @@ function App (){
         
         <BrowserRouter>
         <Routes>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register/>}/>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register/>}/>
         </Routes>
       </BrowserRouter>
     )
