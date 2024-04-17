d45e482e7056af926136fbf34909a45c7a6a1631
Zenbax <sebson@outlook.dk>
2024-04-17 13:21:02 +0200

    Outcommented Pot

--- Cloud/Domain/Model/Pot.cs
+++ Cloud/Domain/Model/Pot.cs
@@ -1,22 +1,22 @@
-namespace YourApiNamespace.Controllers;
-
-public class Pot
-{
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public PotType Type { get; set; }
-    public int HumidityPercent { get; set; }
-    
-    public Pot()
-    {
-    }
-    public Pot(string id, string name, PotType type, int humidityPercent)
-    {
-        Id = id;
-        Name = name;
-        Type = type;
-        HumidityPercent = humidityPercent;
-    }
-}
+// namespace YourApiNamespace.Controllers;
+//
+// public class Pot
+// {
+//     [BsonId]
+//     [BsonRepresentation(BsonType.ObjectId)]
+//     public string Id { get; set; }
+//     public string Name { get; set; }
+//     public PotType Type { get; set; }
+//     public int HumidityPercent { get; set; }
+//     
+//     public Pot()
+//     {
+//     }
+//     public Pot(string id, string name, PotType type, int humidityPercent)
+//     {
+//         Id = id;
+//         Name = name;
+//         Type = type;
+//         HumidityPercent = humidityPercent;
+//     }
+// }
