5e06cb583f0bb099b5ce150c36da5e6bc03ad91e
oumar969 <oammar198@gmail.com>
2024-04-24 14:08:04 +0200

    Plant model class

--- Cloud/Domain/Model/Plant.cs
+++ Cloud/Domain/Model/Plant.cs
@@ -5,4 +5,24 @@ public class Plant
     public string NameOfPlant { get; set; }
     public int SoilMinimumMoisture { get; set; }
     public string ImageUrl { get; set; }
+    
+    public string size { get; set; }
+    
+    public string plantType { get; set; }
+    
+    public HumidityLog humidityLog { get; set; }
+    
+    public Plant()
+    {
+    }
+    
+    public Plant(string nameOfPlant, int soilMinimumMoisture, string imageUrl, string size, string plantType, HumidityLog humidityLog)
+    {
+        NameOfPlant = nameOfPlant;
+        SoilMinimumMoisture = soilMinimumMoisture;
+        ImageUrl = imageUrl;
+        this.size = size;
+        this.plantType = plantType;
+        this.humidityLog = humidityLog;
+    }
 }
