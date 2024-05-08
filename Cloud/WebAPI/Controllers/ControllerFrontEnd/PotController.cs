19394484cf28ab18ce977f26d8898ef7b1ab7af7
EQKim <114055097+EQKim@users.noreply.github.com>
2024-05-08 14:27:26 +0200

    Revert "Update from cloud staging"
    This reverts commit 8770e2b4d7fc487d0626c543de5009c7f44f442f.

--- Cloud/WebAPI/Controllers/ControllerFrontEnd/PotController.cs
+++ Cloud/WebAPI/Controllers/ControllerFrontEnd/PotController.cs
@@ -63,8 +63,29 @@ namespace WebAPI.Controllers.ControllerFrontEnd;
                 return ex.Message;
             }
         }
+        
+        
+        [HttpPut("update/{id}")] 
+        public async Task<IActionResult> UpdatePot(string id, [FromBody] PotUpdatedDto potUpdatedDto)
+        {
+            try
+            {
+                var result = await _potLogic.UpdatePot(id, potUpdatedDto);
+                if (result == "Pot not found")
+                {
+                    return NotFound("Pot not found");
+                }
+                return Ok("Pot updated successfully");
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                return Problem(ex.Message);
+            }
+        }
+        
 
-        [HttpDelete("{id}")]
+        [HttpDelete("delete/{id}")]
         public async Task<ActionResult<string>> Delete(string id)
         {
             try
