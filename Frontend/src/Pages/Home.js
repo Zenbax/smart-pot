92f252ae109e1bde06dd8de0d05653a46c91d205
juli175h <jbramsen1212@gmail.com>
2024-05-13 11:28:47 +0200

    handle pots undefined in home

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -47,7 +47,7 @@ const Home = () =>
                         <h1>Potter</h1>
                     </Row>
                     <Row className=" flex-grow-1 problemRow" >
-                        {pots.length ?
+                        {pots && pots.length ?
                         <PotsList pots={pots}/>
                         :<h1>No pots yet</h1>}
                     </Row>
