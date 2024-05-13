f93d51f6f8df6fc0822b228c31f33da15eaa66b2
Artesh7 <333075@via.dk>
2024-05-13 23:04:42 +0200

    Sender de data iot vil have

--- Cloud/Socket/Models/SensorData.cs
+++ Cloud/Socket/Models/SensorData.cs
@@ -1,10 +1,10 @@
-namespace Socket.Models;
-
 public class SensorData
 {
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public string MachineID { get; set; }
     public double WaterTankLevel { get; set; }
-    public double MeasuredSoilMoisture { get; set; }
-    public double AmountOfWatering { get; set; }
+    public int MeasuredSoilMoisture { get; set; }
+    public int AmountOfWatering { get; set; }
+    public string PotId { get; set; } // Id for den pot, der har denne MachineID
+    public string PlantId { get; set; } // Id for planten i denne pot
 }
--- Cloud/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ Cloud/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b239720dd113f3b5043cc40edb0c88925494b8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
