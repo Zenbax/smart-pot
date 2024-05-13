99a8b31647a06032a1a4a9fa53fea3e54afae9c2
ImMinii <simonbanh@hotmail.com>
2024-05-13 12:57:02 +0200

    Bootstrap logic/layout i potdetails

--- Frontend/src/Pages/PotDetails.js
+++ Frontend/src/Pages/PotDetails.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getPotFromId } from "../API/API_config";
+import PotDataChart from "../Components/PotDataChart";
+
 export default function PotDetails(){
     const {potID} = useParams();
     const [pot, setPot] = useState();
@@ -17,7 +19,63 @@ export default function PotDetails(){
 
 
     return(
-        
-        <h1>{pot?.nameOfPot}</h1>
+    <div class='container'>
+        <div class='row'>
+            <div class="col-md-8">
+                <div class='row'>
+                    <div class='col-md-12'>
+                        <h1>{pot?.NameOfpot} test</h1>
+                    </div>
+                    <div class='col-md-12'>
+                        humidity percentage
+                    </div>
+                </div>
+            </div>
+            <div class='col-md-4'>
+                <div class='row'>
+                    <div class="col-md-9">
+                        <img></img>
+                    </div>
+                    <div class="col-md-3">
+                        water
+                    </div>
+                </div>  
+            </div>
+        <div class='row'>
+            <div class='col-lg-8'>
+            {pot && <PotDataChart potID={potID} />}
+            aaaaaaaaaaaaaaaaaaaaah
+            </div>
+                <div class='col-lg-4'>
+                    <div class='row'>
+                        <div class='col-lg-12'>
+                            <button></button>
+                        </div>
+                        <div class='col-lg-12'>
+                            <button></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-md-7'>
+                <div class='row'>
+                    <div class='col-md-12'>
+                        Plant:
+                    </div>
+                    <div class='col-md-12'>
+                        Minimum Humidity:
+                    </div>
+                    <div class='col-md-12'>
+                        mL per watering:
+                    </div>
+                </div>
+            </div>
+            <div class='col-md-5'>
+                Component: Edit humidity and water
+            </div>
+        </div>
+    </div>
     );
 }
