5b82e4861d97278ba47e49c99509710a4b1138c9
Qraktor <Qraktor@gmail.com>
2024-05-14 12:21:32 +0200

    plantoverview test

--- Frontend/Test/PlantOverview.test.js
+++ Frontend/Test/PlantOverview.test.js
@@ -6,11 +6,18 @@ import '@testing-library/jest-dom';
 test('Render PlantOverView correctly', () => { 
     render (<PlantOverview/>)
 
+    expect(screen.getByText('Back')).toBeInTheDocument();
     expect(screen.getByText('Plants')).toBeInTheDocument();
-
-    //Todo: PlantTemp tror lav en for den selv.
-
-    
+    expect(screen.getAllByTestId('plant-template')[0]).toBeInTheDocument();
+    expect(screen.getByText('Edit Plant')).toBeInTheDocument();
+    expect(screen.getByText('Plant Name:')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Enter plant name")).toBeInTheDocument();
+    expect(screen.getByText('Minimum Soil Moisture:')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Enter minimum moisture")).toBeInTheDocument();
+    expect(screen.getByText('Watering Amount (ml):')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Enter watering amount")).toBeInTheDocument();
+    expect(screen.getByText('Upload a picture')).toBeInTheDocument();
+    expect(screen.getByText('Save Changes')).toBeInTheDocument();    
 
  })
 
