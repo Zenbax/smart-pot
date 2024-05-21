6521799f8326c4720050d786598c2d34dd86fd50
Maja Brixen <331282@via.dk>
2024-05-21 12:04:52 +0200

    SmartPot Watertank warning

--- Frontend/src/Components/SmartPot.js
+++ Frontend/src/Components/SmartPot.js
@@ -15,6 +15,7 @@ const SmartPot = ({potID}) => {
     const [pot, setPot] = useState(null);
     const { setToken } = useAuth();
     const [latestMeasuredSoilData, setLatestMeasuredSoilData] = useState(null);
+    const [warning, setWarning] = useState('');
 
     const handlePotNotFound = () => {
         console.log("Pot not found error");
@@ -27,8 +28,12 @@ const SmartPot = ({potID}) => {
                 if (response && response.success) {
                     setPot(response.pot);
                     const sensorData = response.pot.sensorData;
+                    setError(false);
                     if (sensorData && sensorData.length > 0) {
                         setLatestMeasuredSoilData(sensorData[sensorData.length - 1]);
+                        if (sensorData[sensorData.length - 1].waterTankLevel.currentWaterLevel < 75) {
+                            setWarning(true);
+                        }
                     } else {
                         setLatestMeasuredSoilData(null);
                     }
@@ -52,7 +57,7 @@ const SmartPot = ({potID}) => {
 
     return(
         <Link to={"/pot-details/"+pot.id}>
-        <Container id='smartPodContainer' data-testid='smartPodContainer'>
+        <Container id='smartPodContainer' data-testid='smartPodContainer' className={warning ? 'warningContainer' : ''}>
             <Row>
                 <Col id='bottomCol' md="8">
                     <h1>{pot.nameOfPot}</h1>
@@ -80,71 +85,3 @@ const SmartPot = ({potID}) => {
 
 export default SmartPot
 
-
-
-
-/*import React, { useEffect, useState } from "react";
-import {Container, Row, Col} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import '../Styling/SmartPot.css'
-import waterdropImage from '../images/waterdrop.jpg'
-import { getPotFromId } from "../Util/API_config";
-
-
-const SmartPot = () => {
-    const { potID } = useParams();
-    const navigate = useNavigate();
-    const [pot, setPot] = useState();
-    const [latestMeasuredSoilData, setLatestMeasuredSoilData] = useState(null);
-    const [popupType, setPopupType] = useState('');
-     
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await getPotFromId(potID, handleNotAuthorized, handlePotNotFound);
-                if (response.success) {
-                    setPot(response.pot);
-                    const sensorData = response.pot.sensorData;
-                }
-            } catch (error) {
-                console.error('Error fetching pot data:', error);
-                handlePotNotFound();
-            }
-        };
-        fetchData();
-    }, [potID]);
-
-    if (!pot) {
-        return <div>Loading...</div>;
-    }
-
-
-    return(
-        <Link to={"/pot-details/"+pot.id}>
-        <Container id='smartPodContainer' data-testid='smartPodContainer'>
-            <Row>
-                <Col id='bottomCol' md="8">
-                    <h1>{pot.nameOfPot}</h1>
-                    <h2>{"Plant: "+pot.plant?.nameOfPlant}</h2>
-                    <p>Soil Hydration:</p>
-                    <h1 id='percent'>{latestMeasuredSoilData +"%"}</h1>
-                    <img id='waterdrop' src={waterdropImage}/>
-
-                </Col>
-                <Col md="4">
-                    <img id='smartPotPlant' src={pot.plant?.image} onError={event => {
-                        event.target.src = "https://img.freepik.com/premium-vector/home-plant-potted-plant-isolated-white-flat-vector-illustration_186332-890.jpg"
-                        event.onerror = null
-                        }}/>
-                </Col>
-            </Row>
-        </Container>
-        </Link>
-        
-    );
-}
-
-export default SmartPot
- */
