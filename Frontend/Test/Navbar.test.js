b619bffae81d8c368f7cbd1bfb47f28d901ff649
Maja Brixen <331282@via.dk>
2024-05-21 14:26:21 +0200

    test Navbar

--- Frontend/Test/Navbar.test.js
+++ Frontend/Test/Navbar.test.js
@@ -3,36 +3,65 @@ import {render, screen, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../src/Components/NavBar'
+import { useNavigate, useLocation } from 'react-router-dom';
+
+jest.mock('../src/Util/AuthProvider', () => ({
+    useAuth: () => ({
+      setToken: jest.fn()
+    })
+  }));
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useLocation: jest.fn()
+  }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
 
-test("Navbar is rendered on page", () =>{
+
+test("Navbar is rendered on Login-page", () =>{
+    useLocation.mockReturnValue({ pathname: '/Login' });
+    render(
+        <MemoryRouter>
+            <Navbar />
+        </MemoryRouter>
+    );
+    expect(screen.getByText('Smart-Pot')).toBeInTheDocument();    
+});
+
+
+test("Navbar is rendered on Home-page", () =>{
+    useLocation.mockReturnValue({ pathname: '/Home' });
     render(
         <MemoryRouter>
             <Navbar />
         </MemoryRouter>
     );
     expect(screen.getByText('Smart-Pot')).toBeInTheDocument();
-    expect(screen.getByText('Logout')).toBeInTheDocument();
+    expect(screen.getByText("Logout")).toBeInTheDocument();
 });
 
-
-
-
-
 test("Click on button on the Navbar and navigate to Login-page",() => {
-    const {getByText}  =render(
+
+
+    useLocation.mockReturnValue({ pathname: '/' });
+    const {getByText} =render(
         <Router>
             <Navbar/>
         </Router>
     )
 
-    fireEvent.click(getByText('Logout'));
+    fireEvent.click(screen.getByRole('button', { name: 'Logout' }));
+    expect(window.location.pathname).toBe('/Login');
 
-    expect(window.location.pathname).toBe('/login');
 });
 
 
 test("Navigate to Homepage from Navbar", () =>{
+    useLocation.mockReturnValue({ pathname: '/ConnectPot' });
     const {getByText}  =render(
         <Router>
             <Navbar/>
@@ -42,6 +71,7 @@ test("Navigate to Homepage from Navbar", () =>{
     fireEvent.click(getByText('Smart-Pot'));
 
     expect(window.location.pathname).toBe('/');
+
 
 });
 
