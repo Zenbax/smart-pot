5e974a5254615d402fa5d31b6e2e098af2491d22
Maja Brixen <331282@via.dk>
2024-05-22 12:40:16 +0200

    Test ConnectPot
    Simon og Maja

--- Frontend/Test/ConnectPot.test.js
+++ Frontend/Test/ConnectPot.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ConnectPot from '../src/Pages/ConnectPot';
 import { BrowserRouter } from 'react-router-dom';
+
+
+jest.mock('../src/Util/AuthProvider', () => ({
+  useAuth: () => ({
+    token: 'fake-token',
+    setToken: jest.fn(),
+  })
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(), 
+}));
 
 test('Render ConnectPot correctly', () => { 
     render(<ConnectPot/>);
@@ -10,6 +23,9 @@ test('Render ConnectPot correctly', () => {
     expect(screen.getByText('Connect your Smart-pot')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Smart-pot ID')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Name your pot')).toBeInTheDocument();
+    expect(screen.getByText("Add a plant")).toBeInTheDocument();
+    expect(screen.getByText("Connect Smart-pot")).toBeInTheDocument();
+    expect(screen.getByText("Back")).toBeInTheDocument();
 });
 
 test('Typing into input fields', () => { 
@@ -43,18 +59,33 @@ test('Pop-up when clicking add plant and close when cancel', () => {
     expect(screen.queryByText('Plants')).not.toBeInTheDocument();
 });
 
-test('Selecting a plant template in the pop-up', () => {
-    render(
-        <BrowserRouter>
+test('Selecting a plant template in the pop-up', async () => {
+  render(
+      <BrowserRouter>
           <ConnectPot />
-        </BrowserRouter>
-      );
+      </BrowserRouter>
+  );
 
-    const addButton = screen.getByText('Add a plant');
-    fireEvent.click(addButton);
+  const addButton = screen.getByText('Add a plant');
+  fireEvent.click(addButton);
 
-    const plantTemplate = screen.getAllByTestId('plant-template')[0]; // added a TestId to PlantTemp
-    fireEvent.click(plantTemplate);
+  // Wait for the plant templates to be rendered
+  await waitFor(() => {
+      expect(screen.getAllByTestId('plant-template')).toHaveLength(1); // Adjust length based on the expected number of templates
+  });
 
-    expect(screen.queryByText('Plants')).not.toBeInTheDocument();
+  const plantTemplate = screen.getAllByTestId('plant-template')[0];
+  fireEvent.click(plantTemplate);
+
+  // After clicking on a plant template, the pop-up should close
+  await waitFor(() => expect(screen.queryByText('Plants')).not.toBeInTheDocument());
+
+  // Verify if the selected plant data is updated accordingly here
+  // For example, check if the plant name is displayed
+  await waitFor(() => {
+      expect(screen.getByText(/Plant Name:/i)).toBeInTheDocument();
+  });
 });
+
+
+
