7575dec74350ab28dc2b3e7a0ca703fae6d7f134
ImMinii <simonbanh@hotmail.com>
2024-05-24 09:33:17 +0200

    Watercontainer viser vand igen

--- Frontend/src/Components/WaterContainerChart.js
+++ Frontend/src/Components/WaterContainerChart.js
@@ -1,16 +1,15 @@
-// WaterContainerChart.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../Styling/WaterContainerChart.css';
 
-const WaterContainerChart = ({ currentWaterLevel}) => {
+const WaterContainerChart = ({ currentWaterLevel }) => {
     return (
         <div className="water-container-wrapper">
             <div>
                 {currentWaterLevel} %
             </div>
             <div className="water-container">
-                <div className="water-bar" style={{ height: `currentWaterLevel%` }}></div>
+                <div className="water-bar" style={{ height: `${currentWaterLevel}%` }}></div>
             </div>
             <p className="water-container-text">
                 Water
@@ -21,7 +20,6 @@ const WaterContainerChart = ({ currentWaterLevel}) => {
 
 WaterContainerChart.propTypes = {
     currentWaterLevel: PropTypes.number.isRequired,
-    maxWaterLevel: PropTypes.number.isRequired,
 };
 
 export default WaterContainerChart;
